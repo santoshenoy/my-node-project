6a9696f79b685e060151295bcd111ee4e7f8c0c0
Santosh <santoshenoy@gmail.com>
2022-01-20 13:36:47 -0500

    change

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/hello', (req, res) =>
-    res.send('Hello World!'));
+    res.send('Hello World!!'));
 
 const PORT = 4000;
 app.listen(PORT);
