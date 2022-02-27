f39b79d9a02ee43375975669eb21ce1e599287a0
Santosh <santoshenoy@gmail.com>
2022-02-26 22:26:14 -0500

    serv er.ts Like API

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
+import LikeController from "./controllers/LikeController";
 import mongoose from 'mongoose';
 import BookmarkController from "./controllers/BookmarkController";
 import FollowController from "./controllers/FollowController";
@@ -19,7 +20,9 @@ const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 const followController = FollowController.getInstance(app);
+const followController = FollowController.getInstance(app);
 const messageController = MessageController.getInstance(app);
+const likeController = LikeController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
